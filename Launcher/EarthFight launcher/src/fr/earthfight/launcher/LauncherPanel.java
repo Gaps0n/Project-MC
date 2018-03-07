adeedf2a25c9e0e429a34b693039aea3398e806f
Guillaume <guillaume-cornet07@hotmail.fr>
2018-03-07 10:25:27 +0100

    Window

--- Launcher/EarthFight launcher/src/fr/earthfight/launcher/LauncherPanel.java
+++ Launcher/EarthFight launcher/src/fr/earthfight/launcher/LauncherPanel.java
@@ -1,11 +1,14 @@
 
 package fr.earthfight.launcher;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import fr.theshark34.openlauncherlib.launcher.util.UsernameSaver;
 import fr.theshark34.swinger.Swinger;
 
 @SuppressWarnings("serial")
@@ -13,8 +16,25 @@ public class LauncherPanel extends JPanel {
 	
 	private Image background = Swinger.getResource("background.png");
 	
+	private UsernameSaver saver = new UsernameSaver(Launcher.EF_INFOS);
+	
+	private JTextField usernameField = new JTextField();
+	
+	public LauncherPanel() {
+		this.setLayout(null);
+		
+		usernameField.setForeground(Color.WHITE);
+		usernameField.setFont(usernameField.getFont().deriveFont(20F));
+		usernameField.setCaretColor(Color.WHITE);
+		usernameField.setBorder(null);
+		usernameField.setOpaque(false);
+		usernameField.setBounds(11, 11, 11, 11);
+		this.add(usernameField);
+	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), this);
 	}
 }
